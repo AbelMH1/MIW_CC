209f9722c310730f239da6b1612f25efd6bc04c5
Abel <114153419+AbelMH1@users.noreply.github.com>
2024-11-27 19:29:51 +0100

    Add files via upload

---
+++ analize.js
@@ -0,0 +1,88 @@
+const fileInput = document.getElementById('file-input');
+const clasifyButton = document.getElementById('clasify-button');
+const processButton = document.getElementById('process-button');
+const title = document.getElementById('title');
+const imagePreview = document.getElementById('image-preview');
+
+// const backendUrl = 'http://localhost:3000'
+const backendUrl = 'http://34.173.223.50:3000'
+
+// Función para mostrar la vista previa de la imagen seleccionada
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+
+    // Cuando la imagen se haya leído correctamente, mostrarla en el contenedor
+    reader.onload = function (e) {
+      imagePreview.src = e.target.result;
+      imagePreview.style.display = 'block'; // Mostrar la imagen
+    };
+
+    // Leer el archivo como una URL de datos (base64)
+    reader.readAsDataURL(file);
+  } else {
+    // Si no hay archivo, ocultar la imagen
+    imagePreview.style.display = 'none';
+  }
+});
+
+// Función para enviar una petición de clasificación de imagen
+clasifyButton.addEventListener('click', async () => {
+  const file = fileInput.files[0];
+  if (!file) {
+    alert('Por favor, selecciona una imagen primero.');
+    return;
+  }
+
+  // Crear un FormData para enviar el archivo
+  const formData = new FormData();
+  formData.append('image', file);
+
+  title.textContent = 'Procesando...';
+
+  try {
+    // Enviar la imagen al servidor
+    const response = await fetch(backendUrl + '/analyze-image', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // Obtener el resultado del servidor
+    const data = await response.json();
+    title.textContent = data.message;
+  } catch (error) {
+    console.error('Error al procesar la imagen:', error);
+    title.textContent = 'Error procesando la imagen.';
+  }
+});
+
+// Función para enviar una imágen al proceso de clasificación automático de imágenes
+processButton.addEventListener('click', async () => {
+  const file = fileInput.files[0];
+  if (!file) {
+    alert('Por favor, selecciona una imagen primero.');
+    return;
+  }
+
+  // Crear un FormData para enviar el archivo
+  const formData = new FormData();
+  formData.append('image', file);
+
+  title.textContent = 'Subiendo...';
+
+  try {
+    // Enviar la imagen al servidor
+    const response = await fetch(backendUrl + '/upload-image', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // Obtener el resultado del servidor
+    const data = await response.json();
+    title.textContent = data.message;
+  } catch (error) {
+    console.error('Error al subir la imagen:', error);
+    title.textContent = 'Error subiendo la imagen.';
+  }
+});
---
+++ gallery.js
@@ -0,0 +1,46 @@
+window.onload = async () => {
+  const catImagesList = document.getElementById('cat-images-list');
+  const dogImagesList = document.getElementById('dog-images-list');
+
+  // const backendUrl = 'http://localhost:3000'
+  const backendUrl = 'http://34.173.223.50:3000'
+  const imagesUrl = 'https://miwcloudcomputing.blob.core.windows.net'
+
+  try {
+    // Obtener las imágenes desde el servidor
+    const response = await fetch(backendUrl + '/images');
+    const data = await response.json();
+    console.log(data)
+    console.log(data.catImages)
+
+    // Mostrar las imágenes de "cat"
+    if (data.catImages.length === 0) {
+      catImagesList.textContent = 'Todavía no se han clasificado imágenes de gatos. Prueba a subir una foto o espera un rato.';
+    } else {
+      data.catImages.forEach(image => {
+        const imgElement = document.createElement('img');
+        // imgElement.src = `https://<your-storage-account-name>.blob.core.windows.net/cat/${image}`;
+        imgElement.src = imagesUrl + `/cats/${image}`
+        imgElement.alt = image;
+        catImagesList.appendChild(imgElement);
+      });
+    }
+
+    // Mostrar las imágenes de "dog"
+    if (data.dogImages.length === 0) {
+      dogImagesList.textContent = 'Todavía no se han clasificado imágenes de perros. Prueba a subir una foto o espera un rato.';
+    } else {
+      data.dogImages.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = imagesUrl + `/dogs/${image}`
+        imgElement.alt = image;
+        dogImagesList.appendChild(imgElement);
+      });
+    }
+
+  } catch (error) {
+    console.error('Error al cargar las imágenes:', error);
+    catImagesList.textContent = 'No se pudieron cargar las imágenes de gatos.';
+    dogImagesList.textContent = 'No se pudieron cargar las imágenes de perros.';
+  }
+};
